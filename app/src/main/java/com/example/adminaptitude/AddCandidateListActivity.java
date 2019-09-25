25928210ab4b562d1664bb10531698ae6cfff102
neelima513 <neelimakottapati@gmail.com>
2019-09-25 19:22:11 +0530

    Dialog Box

---
+++ app/src/main/java/com/example/adminaptitude/AddCandidateListActivity.java
@@ -0,0 +1,14 @@
+package com.example.adminaptitude;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCandidateListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_candidate_list);
+    }
+}
--- app/src/main/java/com/example/adminaptitude/MainActivity.java
+++ app/src/main/java/com/example/adminaptitude/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.adminaptitude;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar tool_nav;
     LinearLayout linear_candidate;
+    FloatingActionButton floating_button;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tool_nav=findViewById(R.id.tool_nav);
+        tool_nav=findViewById(R.id.tool_dash);
+        drawerLayout=findViewById(R.id.nav_drawer);
+        floating_button=findViewById(R.id.floating_button);
 
         setSupportActionBar(tool_nav);
 
@@ -30,6 +39,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        floating_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AddCandidateListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ActionBarDrawerToggle  toggle=new ActionBarDrawerToggle(this,drawerLayout,tool_nav,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
 
     }
 }
