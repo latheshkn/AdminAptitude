55598c783322597d1e380d201b4f3392769d46c7
neelima513 <neelimakottapati@gmail.com>
2019-09-25 11:13:32 +0530

    Navbar_Drawer

--- app/src/main/java/com/example/adminaptitude/MainActivity.java
+++ app/src/main/java/com/example/adminaptitude/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.adminaptitude;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar tool_nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tool_nav=findViewById(R.id.tool_nav);
+
+        setSupportActionBar(tool_nav);
+
     }
 }
